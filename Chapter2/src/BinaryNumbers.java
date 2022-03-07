ba0625a9db8e0876bc9f1e4bf096862fdf0aeb28
ridak7 <78869790+ridak7@users.noreply.github.com>
2022-03-07 10:08:08 -0700

    Update BinaryNumbers.java

--- Chapter2/src/BinaryNumbers.java
+++ Chapter2/src/BinaryNumbers.java
@@ -4,6 +4,22 @@
  *CompSci 20
  *Feb 16 2022
  */
+
+Please use the header below
+/*
+
+Program: ????.java          Last Date of this Revision: September 30, 2019
+
+Purpose: An application that uses nested for loops to output a table that diplays number in 
+ multiples of ten.
+
+Author: Your Name, 
+School: CHHS
+Course: Computer Programming ??
+ 
+
+*/
+
 public class BinaryNumbers {
 
 	public static void main(String[] args) {
@@ -104,7 +120,7 @@ public class BinaryNumbers {
 }
 
 
-/*  ScreenDump
+/*  Screen Dump
  * 		
 The following lines convert decimal to binary: 
 The Binary code for the decimal 1 is: 1
