69028cf136ff3e4390dd468b8b9a065d1fc04eff
ridak7 <78869790+ridak7@users.noreply.github.com>
2022-03-07 10:31:07 -0700

    Update RectanglePerimeter.java

--- Chapter3/src/RectanglePerimeter.java
+++ Chapter3/src/RectanglePerimeter.java
@@ -4,6 +4,19 @@
  *CompSci 20
  *Feb 16 2022
  */
+/*
+
+Program: ????.java          Last Date of this Revision: September 30, 2019
+
+Purpose: An application that uses nested for loops to output a table that diplays number in 
+ multiples of ten.
+
+Author: Your Name, 
+School: CHHS
+Course: Computer Programming ??
+ 
+
+*/
 public class RectanglePerimeter {
 	public static void main(String[] args) {
 		
@@ -11,7 +24,7 @@ public class RectanglePerimeter {
 		int width = 4;
 		int length = 13;
 		int perimeter;
-		
+								Ask the user to enter the length and width of this application using Scanner Class
 		perimeter = 2 * length + 2 * width;
 		
 		System.out.println("The perimeter of this rectangle is: " + perimeter);
